4b98c9d36e17b1f2f9f6461afe2acd37c0c8e91a
Mohammad <mirzakhani.mhd@gmail.com>
2021-04-24 07:50:36 +0200

    getkeysByValue Method added to export points with same angle with pivot pont

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -6,14 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
         PatternRecognition patternRecognition = new PatternRecognition();
-        Points point1 = new Points(2,2);
-        Points point2 = new Points(3,1);
-        Points point3 = new Points(1,3);
-        Points point4 = new Points(2,6);
+        Points point1 = new Points(2, 2);
+        Points point2 = new Points(3, 1);
+        Points point3 = new Points(1, 3);
+        Points point4 = new Points(2, 6);
+        Points point5 = new Points(4, 12);
+        Points point6 = new Points(4, 0);
 
 
-
-        patternRecognition.isPattern(3,point1,point2,point3,point4);
+        patternRecognition.isPattern(3, point1, point2, point3, point4,point5,point6);
 
     }
 
--- src/main/java/controller/PatternRecognition.java
+++ src/main/java/controller/PatternRecognition.java
@@ -11,8 +11,11 @@ public class PatternRecognition {
     public double getAngle(Points point1, Points point2) {
         return Math.atan((point2.getY() - point1.getY()) / (point2.getX() - point1.getX()));
     }
+
     public void isPattern(int numberOfPoints, Points... points) {
         Map<String, Double> passedInput = new HashMap<>();
+        Collection<Double> temp = passedInput.values();
+        Set<Set<String>> duplicates = new HashSet<Set<String>>();
 
         if (numberOfPoints > points.length) {
             System.out.println("You Chose parameter N equal to "
@@ -22,17 +25,27 @@ public class PatternRecognition {
                     + " parameters as the input");
         } else {
             for (int i = 0; i < points.length - 1; i++) {
-                for (int j = i + 1; j < points.length; j++) {
-                    passedInput.put(
-                            "Point(" +
-                                    points[j].getX() + "," + points[j].getY() + ")"
-                            , getAngle(points[i], points[j]));
+                System.out.println("Now the PIVOT is " + "Point(" + points[i].getX() + "," + points[i].getY() + ")");
+                for (Points point : points) {
+                    passedInput.put("Point(" + point.getX() + "," + point.getY() + ")", getAngle(points[i], point));
+                }
+                for (Double t : temp) {
+                    if (Collections.frequency(temp, t) > numberOfPoints - 1) {
+                        duplicates.add(getKeysByValue(passedInput, t));
+                    }
+                }
+                if (duplicates.isEmpty()) {
+                    System.out.println("No pattern Founded!");
+                } else {
+                    System.out.println(duplicates);
+                    duplicates.clear();
                 }
             }
         }
-        for(Map.Entry<String,Double> entry: passedInput.entrySet()){
-           System.out.println("Points "+entry.getKey()+" angle "+entry.getValue());
-        }
+    }
+
+    public static <T, E> Set<T> getKeysByValue(Map<T, E> map, E value) {
+        return map.entrySet().stream().filter(e -> Objects.equals(e.getValue(), value)).map(Map.Entry::getKey).collect(Collectors.toSet());
     }
 }
 
